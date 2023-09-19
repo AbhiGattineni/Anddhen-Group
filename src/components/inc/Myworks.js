9b348749b0fe5e5eee2e5bd3919d1390067b49ef
gundlatrinath <trinatgundla@gmail.com>
2023-09-19 21:15:53 +0530

    Updates on work field

--- src/components/inc/Myworks.js
+++ src/components/inc/Myworks.js
@@ -1,30 +1,11 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 import { Card } from '../inc/Card'
-import image from "../images/fullstack.png"
 import { myworks } from '../../dataconfig';
 
-const Myworks = () => {
-    const [active, setActive] = useState(1);
-
+const Myworks = (props) => {
     return (
         <div className='container mb-5'>
-            <div className="d-flex flex-lg-row gap-3 flex-column my-4 ">
-                <button className={`nav-link active px-3 fw-bold rounded-pill border-2 ${active == 1 ? "bg-primary text-white border-0" : "bg-white"}`} onClick={() => setActive(1)} id='nav-portfolio-tab' data-bs-toggle='tab' data-bs-target="#nav-portfolio" type='button' aria-controls='nav-portfolio' aria-selected='true'>
-                    Portfolio or Business Applications
-                </button>
-
-                <button className={`nav-link px-3 fw-bold rounded-pill border-2 ${active == 2 ? "bg-primary text-white border-0" : "bg-white"}`} onClick={() => setActive(2)} id='nav-fullstack-tab' data-bs-toggle='tab' data-bs-target="#nav-fullstack" type='button' aria-controls='nav-fullstack' aria-selected='false'>
-                    Full Stack Applications
-                </button>
-
-                <button className={`nav-link px-3 fw-bold rounded-pill border-2 ${active == 3 ? "bg-primary text-white border-0" : "bg-white"}`} onClick={() => setActive(3)} id='nav-mobile-tab' data-bs-toggle='tab' data-bs-target="#nav-mobile" type='button' aria-controls='nav-mobile' aria-selected='false'>
-                    Mobile Applications
-                </button>
-
-                <button className={`nav-link px-3 fw-bold rounded-pill border-2 ${active == 4 ? "bg-primary text-white border-0" : "bg-white"}`} onClick={() => setActive(4)} id='nav-web-tab' data-bs-toggle='tab' data-bs-target="#nav-web" type='button' aria-controls='nav-web' aria-selected='false'>
-                    Web Applications
-                </button>
-            </div>
+            <p className='fs-5 fw-bold'>{props.title}</p>
             <div className="d-flex flex-row flex-nowrap overflow-auto gap-3">
                 {myworks.map((work) => (
                     <Card image={work.image} title={work.name} description={work.description} link={work.link} />
