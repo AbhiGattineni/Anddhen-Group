8b6469903b583ff8f8d921d1fcbcb21c57286fd9
gundlatrinath <trinatgundla@gmail.com>
2023-09-21 21:18:13 +0530

    ASC page updated

--- src/components/pages/inc/Acs.jsx
+++ src/components/pages/inc/Acs.jsx
@@ -7,8 +7,17 @@ export const Acs = () => {
         <div className="">
             <div className='container mt-3'>
                 <h1 className="main-heading">Anddhen Consulting Services</h1>
-                <Registration />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus facilis aliquam voluptates porro odit laborum iure alias ea neque, nisi debitis in deserunt adipisci dolorum, dolores voluptatem, doloremque maiores dicta commodi ut saepe? Esse ea ex qui, in ullam neque facere porro, quos dolorem sed nostrum ipsa ducimus deleniti fugit! Sint voluptatem maxime dolorum, animi saepe perferendis a, libero, officia voluptatum perspiciatis sit! Doloremque, voluptatibus ex eligendi, repellendus quaerat laudantium corporis, quas est natus nostrum ut possimus? Enim doloremque optio fuga nisi dicta maxime necessitatibus tenetur aperiam. Nobis ab quis, recusandae a qui eveniet quo molestiae ut vero nam quidem!</p>
                 <Pricing />
+                <div className="d-flex gap-3">
+                    <div class="ratio ratio-16x9 w-25">
+                        <iframe src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" title="YouTube video" allowfullscreen></iframe>
+                    </div>
+                    <div class="ratio ratio-16x9 w-25">
+                        <iframe src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" title="YouTube video" allowfullscreen></iframe>
+                    </div>
+                </div>
+                <Registration />
             </div>
         </div>
     )
--- src/components/pages/inc/Registration.jsx
+++ src/components/pages/inc/Registration.jsx
@@ -3,41 +3,53 @@ import { InputField } from '../../inc/InputField'
 
 export const Registration = () => {
     return (
-        <div className='card shadow-sm p-3'>
-            <div className="row gap-4">
-                <div className="col-md-6">
-                    <h5 className=''>Student Registration</h5>
-                    <div className='underline'></div>
-                    <form action="" method='post'>
-                        <InputField name="name" label="Name" placeholder="Full Name" type="text" />
-                        <InputField name="email" label="Email" placeholder="Email" type="email" />
-                        <InputField name="phone" label="Phone" placeholder="Phone" type="tel" />
-                        <InputField name="college" label="College" placeholder="College" type="text" />
-                        <InputField name="reference" label=" Referred by" placeholder="Referrer Name" type="text" />
-                        <div className='d-md-flex my-3 gap-5'>
-                            <label className=''>Job Type</label>
-                            <div className="d-flex gap-2">"
-                                <input type="radio" name="job" value="Internship"/>Internship
-                                <input type="radio" name="job" value="Full Time"/>Full Time
+        <div className="py-3">
+            <h1 className='text-center pb-3'>Registration</h1>
+            <div className='card shadow-sm p-3'>
+                <div className="row gap-4">
+                    <div className="col-md-6">
+                        <h5 className=''>Student Registration</h5>
+                        <div className='underline'></div>
+                        <form action="https://script.google.com/macros/s/AKfycbz1o4WMurk0JJjWFYhLqKpoBqd_ZG2fmKL-pmmHlHgnHDYblvgLNT7GdW9bXLViI9U/exec" method='post'>
+                            <InputField name="name" label="Name" placeholder="Full Name" type="text" />
+                            <InputField name="email" label="Email" placeholder="Email" type="email" />
+                            <InputField name="phone" label="Phone" placeholder="Phone" type="tel" />
+                            <InputField name="college" label="College" placeholder="College" type="text" />
+                            <InputField name="reference" label=" Referred by" placeholder="Referrer Name" type="text" />
+                            <div className='d-md-flex my-3 gap-5'>
+                                <label className=''>Job Type</label>
+                                <div className="d-flex gap-2">
+                                    <input type="radio" name="job" value="Internship" />Internship
+                                    <input type="radio" name="job" value="Full Time" />Full Time
+                                </div>
                             </div>
-                        </div>
-                        <div className='form-group py-3'>
-                            <button type="button " className='btn btn-warning shadow w-100'>Submit</button>
-                        </div>
-                    </form>
-                </div>
-                <div className="col-md-5">
-                    <h5 className=''> Part Timer Registration</h5>
-                    <div className='underline'></div>
-                    <InputField name="Name" label="Name" placeholder="Full Name" type="text" />
-                    <InputField name="Email" label="Email" placeholder="Email" type="email" />
-                    <InputField name="Phone" label="Phone" placeholder="Phone" type="tel" />
-                    <InputField name="College" label="College" placeholder="College" type="text" />
-                    <InputField name="Referrer" label="Referred by" placeholder="Referrer Name" type="text" />
-                    <InputField name="status" label="Current Status" placeholder="Current Status" type="text" />
-                    <div className='form-group py-3'>
-                        <button type="button " className='btn btn-warning shadow w-100'>Submit</button>
+                            <div className='form-group py-3'>
+                                <button type="button " className='btn btn-warning shadow w-100'>Submit</button>
+                            </div>
+                        </form>
                     </div>
+                    <div className="col-md-5">
+                        <h5 className=''> Part Timer Registration</h5>
+                        <div className='underline'></div>
+                        <form action="" method='post'>
+                            <InputField name="name" label="Name" placeholder="Full Name" type="text" />
+                            <InputField name="email" label="Email" placeholder="Email" type="email" />
+                            <InputField name="phone" label="Phone" placeholder="Phone" type="tel" />
+                            <InputField name="college" label="College" placeholder="College" type="text" />
+                            <InputField name="reference" label="Referred by" placeholder="Referrer Name" type="text" />
+                            <InputField name="status" label="Current Status" placeholder="Current Status" type="text" />
+                            <div className='form-group py-3'>
+                                <button type="button " className='btn btn-warning shadow w-100'>Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div className="border-top py-2">
+                    <h5 className='main-heading'>Address Information</h5>
+                    <div className='underline'></div>
+                    <p>Hyderabad, Telangana-521175</p>
+                    <p>Phone: +91 8801043608</p>
+                    <p>Email: email@domain.com</p>
                 </div>
             </div>
         </div>
