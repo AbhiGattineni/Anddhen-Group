4c0ecb181fdd8a4deeaece7c4ef9185c61a7c3ae
gundlatrinath <trinatgundla@gmail.com>
2023-10-04 20:30:50 +0530

    Quiz Page created

--- src/components/pages/inc/Acs.jsx
+++ src/components/pages/inc/Acs.jsx
@@ -16,9 +16,11 @@ export const Acs = () => {
                     <h3 className='main-heading'>Practice Test</h3>
                     <div className='underline mx-auto'></div>
                 </div>
-                <div className="w-50">
-                    <h3>Job Application Video Checklist: Test Your Understanding</h3>
-                    <p>This quiz aims to test how well you've grasped the key concepts presented in those videos. Successfully completing the quiz demonstrates your commitment and readiness for the job application process. Ensure you've watched all the videos thoroughly before attempting. Good luck!</p>
+                <div className="row">
+                    <div className="col-md-6 col-lg-6 col-12">
+                        <h3>Job Application Video Checklist: Test Your Understanding</h3>
+                        <p>This quiz aims to test how well you've grasped the key concepts presented in those videos. Successfully completing the quiz demonstrates your commitment and readiness for the job application process. Ensure you've watched all the videos thoroughly before attempting. Good luck!</p>
+                    </div>
                 </div>
                 <Link to={"/test"} className='btn btn-warning shadow'>Take Test</Link>
                 <Registration />
