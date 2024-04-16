31e8fc555bf1cc43394cd5c2361dd5de817e700e
gundlatrinath <trinatgundla@gmail.com>
2024-04-16 20:11:13 +0530

    images added to the slider

--- src/components/organisms/Slider.js
+++ src/components/organisms/Slider.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import Slider1 from '../images/web.jpg';
-import Slider2 from '../images/fullstack.png';
-import Slider3 from '../images/app.jpg';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
+import Slider1 from '../images/slider-1.png';
+import Slider2 from '../images/slider-2.jpg';
+import Slider3 from '../images/fullstack.png';
+import Slider4 from '../images/slider-4.jpg';
+import Slider5 from '../images/slider-5.jpg';
+// import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
 function Slider() {
@@ -12,27 +14,43 @@ function Slider() {
                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
             </div>
             <div className="carousel-inner">
                 <div className="carousel-item active">
-                    <LazyLoadImage effect="blur" src={Slider1} className="d-block w-100" alt="..." />
-                    <div className="carousel-caption d-none d-md-block">
-                        <h5>First slide label</h5>
-                        <p>Some representative placeholder content for the first slide.</p>
+                    <img src={Slider1} className="d-block w-100 h-100 carousel-image" alt="Slide 1" />
+                    <div className="carousel-caption d-none d-md-block text-black">
+                        <h5>Anddhen Marketing Services</h5>
+                        <p>Empowering your brand's online presence through strategic social media marketing solutions with Anddhen Marketing Services.</p>
                     </div>
                 </div>
                 <div className="carousel-item">
-                    <LazyLoadImage effect="blur" src={Slider2} className="d-block w-100" alt="..." />
-                    <div className="carousel-caption d-none d-md-block">
-                        <h5>Second slide label</h5>
-                        <p>Some representative placeholder content for the second slide.</p>
+                    <img src={Slider2} className="d-block w-100 h-100 carousel-image" alt="Slide 2" />
+                    <div className="carousel-caption d-none d-md-block text-white">
+                        <h5>Anddhen Consulting Services</h5>
+                        <p className='text-white'>Guiding aspiring students through the complex process of studying abroad with personalized consulting services from Anddhen Consulting.</p>
                     </div>
                 </div>
                 <div className="carousel-item">
-                    <LazyLoadImage effect="blur" src={Slider3} className="d-block w-100" alt="..." />
+                    <img src={Slider3} className="d-block w-100 h-100 carousel-image" alt="Slide 3" />
                     <div className="carousel-caption d-none d-md-block">
-                        <h5>Third slide label</h5>
-                        <p>Some representative placeholder content for the third slide.</p>
+                        <h5>Anddhen Software Services</h5>
+                        <p>Revolutionizing businesses with cutting-edge software solutions tailored to their unique needs, Anddhen Software Services drives innovation and efficiency.</p>
+                    </div>
+                </div>
+                <div className="carousel-item">
+                    <img src={Slider4} className="d-block w-100 h-100 carousel-image" alt="Slide 4" />
+                    <div className="carousel-caption d-none d-md-block">
+                        <h5>Anddhen Philanthropy Services</h5>
+                        <p className=' text-white'>Empowering positive change and social impact through strategic philanthropic initiatives, Anddhen Philanthropy Services champions meaningful causes worldwide.</p>
+                    </div>
+                </div>
+                <div className="carousel-item">
+                    <img src={Slider5} className="d-block w-100 h-100 carousel-image" alt="Slide 5" />
+                    <div className="carousel-caption d-none d-md-block">
+                        <h5>Anddhen Trading & Investiment</h5>
+                        <p className=' text-white'>Unlocking financial opportunities and maximizing returns through expert trading and investment strategies, Anddhen Trading & Investment leads the way in wealth management.</p>
                     </div>
                 </div>
             </div>
