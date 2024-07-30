27b9a771114e8db8b7f14ff159262128f460e7af
AbhiGattineni <abhishekgattineni@gmail.com>
2024-07-29 20:16:42 -0700

    chnages in aps format

--- src/components/pages/inc/Aps.jsx
+++ src/components/pages/inc/Aps.jsx
@@ -13,28 +13,49 @@ export const Aps = () => {
             className="rounded"
             src={logo}
             alt="Card image cap"
-            style={{ height: '60vh', width: '60vw' }} //change this for image height and width
+            style={{ height: '50vh', width: '60vw' }} //change this for image height and width
         />
             </div>
             
             <section className='p-2 bg-c-light border-top'>
                 <div className='container'>
-                    <h5 className='pt-5 main-heading text-center'>Education</h5>
-                    <div className="underline mx-auto"></div>
-                    <p className='p-2' style={{ textAlign: 'justify' }}>
-            The <strong>ANDDHEN - JAYA LAKSHMI FOUNDATION</strong> is a labor of love, a profound homage to my cherished grandmother, <strong>Jaya Lakshmi</strong>, who is alive and continues to inspire us. As a devoted government school headmistress, she dedicated her life to nurturing and educating countless children, instilling in them the values of knowledge, integrity, and compassion. Her relentless commitment to education and her selfless care for others have profoundly influenced my life, guiding me with her wisdom and unwavering support. Now, in her honor, we have established this foundation to continue her life's work, to reach out to those children whose dreams are hindered by financial constraints, and to light the path to a brighter future through education.
-        </p>
-        <p className='p-2' style={{ textAlign: 'justify' }}>
-            Education is the most powerful tool we can offer to uplift lives and break the cycle of poverty. The <strong>ANDDHEN - JAYA LAKSHMI FOUNDATION</strong> is dedicated to this noble cause, striving to ensure that no child is deprived of the opportunity to learn and grow due to financial hardship. By providing scholarships, resources, and support, we aim to transform the lives of these young learners, giving them the chance to achieve their fullest potential and contribute positively to society. We are not just funding education; we are nurturing dreams, fostering hope, and building a foundation for a brighter tomorrow.
-        </p>
-        <p className='p-2' style={{ textAlign: 'justify' }}>
-            The impact of <strong>Jaya Lakshmi</strong>'s teachings goes beyond the classroom. Her lessons of kindness, resilience, and dedication resonate deeply within me and countless others who had the privilege of knowing her. Through the <strong>ANDDHEN - JAYA LAKSHMI FOUNDATION</strong>, we aspire to extend her legacy of compassion and generosity to children who need it the most. Each contribution to this foundation is a step toward creating a world where every child has access to the education they deserve, enabling them to rise above their circumstances and thrive.
-        </p>
-        <p className='p-2' style={{ textAlign: 'justify' }}>
-            We invite you to join us on this heartfelt journey. Your generosity can change the course of a child's life, offering them hope, possibilities, and a future filled with promise. If you feel moved to support our mission or wish to learn more about how you can make a difference, please contact us. Together, we can keep <strong>Jaya Lakshmi</strong>'s legacy alive and create a world where every child's educational aspirations are within reach.
-        </p>
+                    <div className="row justify-content-center">
+                        <div className="col-12 col-md-10 col-lg-9">
+                            <h5 className='pt-5 main-heading text-center'>Education</h5>
+                            <div className='underline mx-auto'></div>
+                            <p className='p-2' style={{ textAlign: 'justify' }}>
+                                <strong>Dear Friends,</strong>
+                            </p>
+                            <p className='p-2' style={{ textAlign: 'justify' }}>
+                                The <strong>ANDDHEN - JAYA LAKSHMI FOUNDATION</strong> is a labor of love, a profound homage to my cherished grandmother, <strong>Jaya Lakshmi</strong>, who is alive and continues to inspire us. As a devoted government school headmistress, she dedicated her life to nurturing and educating countless children, instilling in them the values of knowledge, integrity, and compassion. Her relentless commitment to education and her selfless care for others have profoundly influenced my life, guiding me with her wisdom and unwavering support.
+                            </p>
+                            <p className='p-2' style={{ textAlign: 'justify' }}>
+                                Now, in her honor, we have established this foundation to continue her life's work, to reach out to those children whose dreams are hindered by financial constraints, and to light the path to a brighter future through education. Education is the most powerful tool we can offer to uplift lives and break the cycle of poverty. The <strong>ANDDHEN - JAYA LAKSHMI FOUNDATION</strong> is dedicated to this noble cause, striving to ensure that no child is deprived of the opportunity to learn and grow due to financial hardship.
+                            </p>
+                            <p className='p-2' style={{ textAlign: 'justify' }}>
+                                By providing scholarships, resources, and support, we aim to transform the lives of these young learners, giving them the chance to achieve their fullest potential and contribute positively to society. We are not just funding education; we are nurturing dreams, fostering hope, and building a foundation for a brighter tomorrow.
+                            </p>
+                            <p className='p-2' style={{ textAlign: 'justify' }}>
+                                The impact of <strong>Jaya Lakshmi</strong>'s teachings goes beyond the classroom. Her lessons of kindness, resilience, and dedication resonate deeply within me and countless others who had the privilege of knowing her. Through the <strong>ANDDHEN - JAYA LAKSHMI FOUNDATION</strong>, we aspire to extend her legacy of compassion and generosity to children who need it the most.
+                            </p>
+                            <p className='p-2' style={{ textAlign: 'justify' }}>
+                                Each contribution to this foundation is a step toward creating a world where every child has access to the education they deserve, enabling them to rise above their circumstances and thrive. We invite you to join us on this heartfelt journey. Your generosity can change the course of a child's life, offering them hope, possibilities, and a future filled with promise.
+                            </p>
+                            <p className='p-2' style={{ textAlign: 'justify' }}>
+                                If you feel moved to support our mission or wish to learn more about how you can make a difference, please contact us. Together, we can keep <strong>Jaya Lakshmi</strong>'s legacy alive and create a world where every child's educational aspirations are within reach.
+                            </p>
+                            <p className='px-2' style={{ textAlign: 'justify' }}>
+                                With heartfelt gratitude,
+                            </p>
+                            <p className='px-2' style={{ textAlign: 'justify' }}>
+                                <strong>Founder,</strong><br />
+                                <strong>ANDDHEN - JAYA LAKSHMI FOUNDATION</strong>
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </section>
+
         </div>
     )
 }
