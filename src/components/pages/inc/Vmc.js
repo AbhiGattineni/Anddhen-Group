3522000f663d82ba9b70b354c69a5a3c1be257c2
AbhiGattineni <abhishekgattineni@gmail.com>
2024-07-30 19:04:52 -0700

    Add Prettier and ESLint configuration

--- src/components/pages/inc/Vmc.js
+++ src/components/pages/inc/Vmc.js
@@ -11,19 +11,19 @@ function Vmc() {
                     </div>
                     <div className='col-md-4 text-center'>
                         <h6>Our Vision</h6>
-                        <p>
+                        <p style={{ textAlign: 'justify' }}>
                         To be a beacon of innovation and integrity in the global marketplace, transforming businesses and communities through cutting-edge technology, strategic investment, and dedicated philanthropy. We envision a world where our conglomerate not only leads in providing comprehensive solutions across various sectors but also makes a substantial impact on societal advancement and sustainability.
                         </p>
                     </div>
                     <div className='col-md-4 text-center'>
                         <h6>Our Mission</h6>
-                        <p>
+                        <p style={{ textAlign: 'justify' }}>
                         Our mission is to empower our clients and communities by delivering exceptional services across software development, staffing and consulting, trading and investment, and marketing. We are committed to fostering growth, efficiency, and success for our partners while upholding our commitment to social responsibility. Through our expertise and philanthropic initiatives, we strive to create opportunities that drive positive change and contribute to a better future.
                         </p>
                     </div>
                     <div className='col-md-4 text-center'>
                         <h6>Our Values</h6>
-                        <p>
+                        <p style={{ textAlign: 'justify' }}>
                         <ul>
   <strong>Innovation:</strong> Leading with creativity.<br/>
   <strong>Integrity:</strong> Upholding honesty.<br/>
