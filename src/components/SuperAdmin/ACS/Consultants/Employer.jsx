10ec00d840811e68b9041ca794c0496089e9cf82
sampreeth <saisampreeth18@gmail.com>
2025-02-04 22:38:50 +0530

    resolved error in employee managment

--- src/components/SuperAdmin/ACS/Consultants/Employer.jsx
+++ src/components/SuperAdmin/ACS/Consultants/Employer.jsx
@@ -11,15 +11,24 @@ import {
   Button,
   Paper,
   Typography,
+  CircularProgress,
   Grid,
   Pagination,
 } from '@mui/material';
-import { Edit, Delete, Add, Search } from '@mui/icons-material';
-import { useFetchData } from 'src/react-query/useFetchApis';
+import { Edit, Save, Delete, Add, Search } from '@mui/icons-material';
+import {
+  useFetchData,
+  useAddData,
+  useDeleteData,
+  useUpdateData,
+} from 'src/react-query/useFetchApis';
+import { useQueryClient } from 'react-query';
 import Toast from 'src/components/organisms/Toast';
 
 const Employer = () => {
   const [search, setSearch] = useState('');
+  const [editRowId, setEditRowId] = useState(null);
+  const [selectedId, setSelectedId] = useState(null);
   const [editValues, setEditValues] = useState({ name: '', address: '' });
   const [newRow, setNewRow] = useState({ name: '', address: '' });
   const [toast, setToast] = useState({
@@ -29,21 +38,134 @@ const Employer = () => {
   });
   const [page, setPage] = useState(1);
   const rowsPerPage = 15;
+  const queryClient = useQueryClient();
 
   const { data = [] } = useFetchData('employer', '/employers/');
+
+  const handleSearch = (e) => setSearch(e.target.value);
+
+  const handleEditId = (id) => {
+    const row = data.find((row) => row.id === id);
+    setEditValues({ name: row.name, address: row.address });
+    setEditRowId(id);
+  };
+
+  const { mutate: updateEmployer, isLoading: isUpdating } = useUpdateData(
+    'employer',
+    `/employers/${editRowId}/`,
+  );
+
+  const handleSave = () => {
+    updateEmployer(editValues, {
+      onSuccess: () => {
+        queryClient.invalidateQueries('employer');
+        setToast({
+          show: true,
+          message: 'Employer updated successfully!',
+          color: '#82DD55',
+        });
+        setTimeout(
+          () => setToast({ show: false, message: '', color: undefined }),
+          3000,
+        );
+        setEditRowId(null);
+      },
+      onError: (error) => {
+        console.error('An error occurred:', error);
+        setToast({
+          show: true,
+          message: 'Something went wrong!',
+          color: '#E23636',
+        });
+        setTimeout(
+          () => setToast({ show: false, message: '', color: undefined }),
+          3000,
+        );
+      },
+    });
+  };
+
+  const handleInputChange = (e, field) => {
+    setEditValues((prev) => ({ ...prev, [field]: e.target.value }));
+  };
+
+  const { mutate: addEmployer } = useAddData('employer', '/employers/');
+  const handleAddRow = () => {
+    addEmployer(newRow, {
+      onSuccess: () => {
+        queryClient.invalidateQueries('employer');
+        setNewRow({ name: '', address: '' });
+        setToast({
+          show: true,
+          message: 'Employer added successfully!',
+          color: '#82DD55',
+        });
+        setTimeout(
+          () => setToast({ show: false, message: '', color: undefined }),
+          3000,
+        );
+      },
+      onError: (error) => {
+        console.error('An error occurred:', error);
+        setToast({
+          show: true,
+          message: 'Something went wrong!',
+          color: '#E23636',
+        });
+        setTimeout(
+          () => setToast({ show: false, message: '', color: undefined }),
+          3000,
+        );
+      },
+    });
+  };
+
+  const { mutate: deleteEmployer, isLoading: isDeleting } = useDeleteData(
+    'employer',
+    `/employers/${selectedId}/`,
+  );
+  const handleDelete = (id) => {
+    setSelectedId(id);
+    deleteEmployer(null, {
+      onSuccess: () => {
+        queryClient.invalidateQueries('employer');
+        setToast({
+          show: true,
+          message: 'Employer deleted successfully!',
+          color: '#82DD55',
+        });
+        setTimeout(
+          () => setToast({ show: false, message: '', color: undefined }),
+          3000,
+        );
+      },
+      onError: (error) => {
+        console.error('An error occurred:', error);
+        setToast({
+          show: true,
+          message: 'Something went wrong!',
+          color: '#E23636',
+        });
+        setTimeout(
+          () => setToast({ show: false, message: '', color: undefined }),
+          3000,
+        );
+      },
+    });
+  };
+
   const filteredData = data.filter(
     (row) =>
       row.name.toLowerCase().includes(search.toLowerCase()) ||
       row.address.toLowerCase().includes(search.toLowerCase()),
   );
 
-  const handleSearch = (e) => setSearch(e.target.value);
-  const handlePageChange = (event, value) => setPage(value);
-
   const paginatedData = filteredData.slice(
     (page - 1) * rowsPerPage,
     page * rowsPerPage,
   );
+
+  const handlePageChange = (event, value) => setPage(value);
 
   return (
     <Paper style={{ padding: 20 }}>
@@ -59,9 +181,7 @@ const Employer = () => {
             onChange={handleSearch}
             fullWidth
             margin="normal"
-            InputProps={{
-              startAdornment: <Search />,
-            }}
+            InputProps={{ startAdornment: <Search /> }}
           />
         </Grid>
       </Grid>
@@ -94,7 +214,12 @@ const Employer = () => {
                 </TableCell>
               ))}
               <TableCell>
-                <Button color="primary" variant="contained" startIcon={<Add />}>
+                <Button
+                  onClick={handleAddRow}
+                  color="primary"
+                  variant="contained"
+                  startIcon={<Add />}
+                >
                   Add
                 </Button>
               </TableCell>
@@ -102,14 +227,40 @@ const Employer = () => {
             {paginatedData.map((row) => (
               <TableRow key={row.id}>
                 {['name', 'address'].map((field) => (
-                  <TableCell key={field}>{row[field]}</TableCell>
+                  <TableCell key={field}>
+                    {editRowId === row.id ? (
+                      <TextField
+                        value={editValues[field]}
+                        onChange={(e) => handleInputChange(e, field)}
+                        fullWidth
+                      />
+                    ) : (
+                      row[field]
+                    )}
+                  </TableCell>
                 ))}
                 <TableCell>
-                  <IconButton color="primary">
-                    <Edit />
-                  </IconButton>
-                  <IconButton color="secondary">
-                    <Delete />
+                  {editRowId === row.id ? (
+                    <IconButton onClick={handleSave} color="primary">
+                      {isUpdating ? <CircularProgress size={24} /> : <Save />}
+                    </IconButton>
+                  ) : (
+                    <IconButton
+                      onClick={() => handleEditId(row.id)}
+                      color="primary"
+                    >
+                      <Edit />
+                    </IconButton>
+                  )}
+                  <IconButton
+                    onClick={() => handleDelete(row.id)}
+                    color="secondary"
+                  >
+                    {isDeleting && selectedId === row.id ? (
+                      <CircularProgress size={24} />
+                    ) : (
+                      <Delete />
+                    )}
                   </IconButton>
                 </TableCell>
               </TableRow>
@@ -117,15 +268,12 @@ const Employer = () => {
           </TableBody>
         </Table>
       </TableContainer>
-      {/* Pagination */}
-      <Grid container justifyContent="center" sx={{ marginTop: 2 }}>
-        <Pagination
-          count={Math.ceil(filteredData.length / rowsPerPage)}
-          page={page}
-          onChange={handlePageChange}
-          color="primary"
-        />
-      </Grid>
+      <Pagination
+        count={Math.ceil(filteredData.length / rowsPerPage)}
+        page={page}
+        onChange={handlePageChange}
+        sx={{ marginTop: 2, display: 'flex', justifyContent: 'center' }}
+      />
       <Toast
         show={toast.show}
         message={toast.message}
