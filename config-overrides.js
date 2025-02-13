f181c39aaac6fe8b9c2cd7509ea50667ce057f09
gundlatrinath <trinatgundla@gmail.com>
2025-02-13 18:06:54 +0530

    deploy fix

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,11 @@
 module.exports = function override(config) {
-    config.resolve = {
-      ...config.resolve,
-      alias: {
-        ...config.resolve.alias,
-        "react/jsx-runtime": "react/jsx-runtime.js",
-        "react/jsx-dev-runtime": "react/jsx-dev-runtime.js"
-      }
-    };
-    return config;
+  config.resolve = {
+    ...config.resolve,
+    alias: {
+      ...config.resolve.alias,
+      'react/jsx-runtime': 'react/jsx-runtime.js',
+      'react/jsx-dev-runtime': 'react/jsx-dev-runtime.js',
+    },
   };
-  
+  return config;
+};
