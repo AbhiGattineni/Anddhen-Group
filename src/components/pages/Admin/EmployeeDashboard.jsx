c1eaa5cb83b17b26bb3e1b84ca81a33c0ae0d09c
gundlatrinath <trinatgundla@gmail.com>
2025-03-02 08:35:43 +0530

    status form updated

--- src/components/pages/Admin/EmployeeDashboard.jsx
+++ src/components/pages/Admin/EmployeeDashboard.jsx
@@ -7,14 +7,56 @@ import { useQueryClient } from 'react-query';
 import useAuthStore from 'src/services/store/globalStore';
 import AssignCards from './AssignCards';
 import { adminPlates } from 'src/dataconfig';
+import {
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from '@mui/material';
+
+const subsidiaryOptions = ['ACS', 'ASS', 'ATI', 'ANS', 'AMS'];
+
+const initialFormState = {
+  user_id: auth?.currentUser?.uid || '',
+  user_name: auth?.currentUser?.displayName || '',
+  subsidary: '',
+  date: '',
+  description: '',
+  AMS: { source: '' },
+  ACS: {
+    studentName: '',
+    studentId: '',
+    applicationsAppliedSearched: 0,
+    applicationsAppliedSaved: 0,
+    easyApply: 0,
+    recruiterDirectMessages: '',
+    connectMessages: '',
+    reason: '',
+  },
+  ASS: { ticket_link: '', github_link: '' },
+  ATI: {
+    account_name: '',
+    stock_name: '',
+    stock_quantity: 0,
+    stock_value: 0.0,
+    transaction_type: '',
+    total_current_amount: 0.0,
+  },
+  ANS: {
+    pickup_location: '',
+    pickup_contact: '',
+    dropoff_location: '',
+    dropoff_contact: '',
+    distance_travelled: 0.0,
+    whatsapp_group_number: '',
+  },
+};
 
 export const EmployeeDashboard = () => {
   const empName = '';
-  const [formValues, setFormValues] = useState({
-    name: '',
-    date: '',
-    status: '',
-  });
+  const [formValues, setFormValues] = useState(initialFormState);
   const [msgResponse, setMsgResponse] = useState(null);
   const [disableInputs, setDisableInputs] = useState(false);
   const [showEdit, setShowEdit] = useState(false);
@@ -66,7 +108,8 @@ export const EmployeeDashboard = () => {
     if (auth.currentUser && auth.currentUser.displayName) {
       setFormValues({
         ...formValues,
-        name: auth.currentUser.displayName,
+        user_id: auth.currentUser.uid,
+        user_name: auth.currentUser.displayName,
       });
     }
   }, [auth.currentUser]);
@@ -79,29 +122,67 @@ export const EmployeeDashboard = () => {
     return `${year}-${month}-${day}`;
   };
 
+  function restructureObject(flatObj) {
+    return {
+      user_id: flatObj.user_id || '',
+      user_name: flatObj.user_name || '',
+      subsidary: flatObj.subsidary || '',
+      date: flatObj.date || '',
+      description: flatObj.description || null,
+      AMS: { source: flatObj.source || null },
+      ACS: {
+        studentName: flatObj.studentName || null,
+        studentId: flatObj.studentId || null,
+        applicationsAppliedSearched: flatObj.applicationsAppliedSearched || 0,
+        applicationsAppliedSaved: flatObj.applicationsAppliedSaved || 0,
+        easyApply: flatObj.easyApply || 0,
+        recruiterDirectMessages: flatObj.recruiterDirectMessages || null,
+        connectMessages: flatObj.connectMessages || null,
+        reason: flatObj.reason || null,
+      },
+      ASS: {
+        ticket_link: flatObj.ticket_link || null,
+        github_link: flatObj.github_link || null,
+      },
+      ATI: {
+        account_name: flatObj.account_name || null,
+        stock_name: flatObj.stock_name || null,
+        stock_quantity: flatObj.stock_quantity || 0,
+        stock_value: flatObj.stock_value || 0.0,
+        transaction_type: flatObj.transaction_type || null,
+        total_current_amount: flatObj.total_current_amount || 0.0,
+      },
+      ANS: {
+        pickup_location: flatObj.pickup_location || null,
+        pickup_contact: flatObj.pickup_contact || null,
+        dropoff_location: flatObj.dropoff_location || null,
+        dropoff_contact: flatObj.dropoff_contact || null,
+        distance_travelled: flatObj.distance_travelled || 0.0,
+        whatsapp_group_number: flatObj.whatsapp_group_number || null,
+      },
+    };
+  }
+
   useEffect(() => {
     const formattedSelectedDate = formatDate(selectedAcsStatusDate);
     setMsgResponse(null);
     if (data && selectedAcsStatusDate) {
       let found = false;
-      data.forEach((status) => {
+      for (const status of data) {
         if (formattedSelectedDate === status.date) {
           found = true;
           setDisableInputs(true);
-          setFormValues({
-            name: auth.currentUser.displayName,
-            date: status.date,
-            status: status.status,
-          });
-          return;
+          const formStatus = restructureObject(status);
+          setFormValues(formStatus);
+          break;
         }
-      });
+      }
       if (!found) {
         setDisableInputs(false);
         setFormValues({
-          name: auth.currentUser.displayName,
-          date: '',
-          status: '',
+          ...initialFormState,
+          user_name: auth.currentUser.displayName,
+          user_id: auth.currentUser.uid,
         });
       }
     }
@@ -119,18 +200,67 @@ export const EmployeeDashboard = () => {
     }
   }, [selectedAcsStatusDate, data]);
 
-  const handleInputChange = (e) => {
-    const { id, value } = e.target;
-    setFormValues({
-      ...formValues,
-      [id]: value,
-    });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name.includes('.')) {
+      const [subsidiary, field] = name.split('.');
+      setFormValues((prev) => ({
+        ...prev,
+        [subsidiary]: { ...prev[subsidiary], [field]: value },
+      }));
+    } else {
+      setFormValues((prev) => ({ ...prev, [name]: value }));
+    }
+  };
+
+  const renderSubsidiaryFields = () => {
+    if (!formValues.subsidary) return null;
+
+    const selectedSubsidiary = formValues.subsidary;
+    const fields = initialFormState[selectedSubsidiary];
+
+    if (!fields) return null; // Ensure fields exist
+
+    // Initialize formValues[selectedSubsidiary] if undefined
+    if (!formValues[selectedSubsidiary]) {
+      formValues[selectedSubsidiary] = { ...fields };
+    }
+
+    return Object.keys(fields).map((key) => (
+      <Grid item xs={12} sm={6} key={key}>
+        <TextField
+          fullWidth
+          label={key
+            .replace(/_/g, ' ')
+            .replace(/\b\w/g, (char) => char.toUpperCase())}
+          name={`${selectedSubsidiary}.${key}`}
+          value={formValues[selectedSubsidiary]?.[key] || ''}
+          onChange={handleChange}
+          disabled={disableInputs}
+          variant="outlined"
+        />
+      </Grid>
+    ));
+  };
+
+  const flattenObject = (obj) => {
+    return Object.keys(obj).reduce((acc, key) => {
+      if (
+        typeof obj[key] === 'object' &&
+        obj[key] !== null &&
+        !Array.isArray(obj[key])
+      ) {
+        return { ...acc, ...obj[key] }; // Spread nested object properties
+      }
+      acc[key] = obj[key]; // Keep top-level properties
+      return acc;
+    }, {});
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const requiredFields = ['date', 'status'];
+      const requiredFields = ['date', 'subsidary'];
       const missingField = requiredFields.find((field) => !formValues[field]);
 
       if (missingField) {
@@ -139,8 +269,8 @@ export const EmployeeDashboard = () => {
           case 'date':
             missingFieldName = 'Date';
             break;
-          case 'status':
-            missingFieldName = 'Status';
+          case 'subsidary':
+            missingFieldName = 'Subsidary';
             break;
           default:
             break;
@@ -151,12 +281,16 @@ export const EmployeeDashboard = () => {
         return;
       }
 
-      const postStatus = {
-        user_id: auth.currentUser.uid,
-        user_name: formValues.name,
-        date: formValues.date,
-        status: formValues.status,
-      };
+      // const postStatus = {
+      //   user_id: auth.currentUser.uid,
+      //   user_name: formValues.name,
+      //   date: formValues.date,
+      //   status: formValues.status,
+      // };
+
+      const postStatus = flattenObject(formValues);
+
+      console.log(postStatus);
 
       let response;
       try {
@@ -168,17 +302,17 @@ export const EmployeeDashboard = () => {
           setMsgResponse(response.message);
         }
 
-        queryClient.invalidateQueries(['calendarData', postStatus]);
+        queryClient.invalidateQueries(['calendarData', formValues]);
       } catch (error) {
-        setMsgResponse('Something went wrong');
+        setMsgResponse(response.message);
         console.error('Mutation error:', error);
       }
 
       if (!showEdit) {
         setFormValues({
-          name: auth.currentUser.displayName,
-          date: '',
-          status: '',
+          ...initialFormState,
+          user_name: auth.currentUser.displayName,
+          user_id: auth.currentUser.uid,
         });
       }
     } catch (error) {
@@ -209,49 +343,66 @@ export const EmployeeDashboard = () => {
               {msgResponse}
             </div>
           )}
-          <div className="row">
-            <div className="col-6 col-md-3">
-              <div className="mb-3">
-                <input
-                  type="text"
-                  className="form-control"
-                  id="name"
-                  placeholder="Name"
-                  value={formValues.name}
-                  onChange={handleInputChange}
-                  disabled
-                />
-              </div>
-            </div>
-            <div className="col-6 col-md-3">
-              <div className="mb-3">
-                <input
-                  type="date"
-                  className="form-control"
-                  id="date"
-                  placeholder="Date"
-                  value={formValues.date}
-                  onChange={handleInputChange}
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                label="User Name"
+                name="user_name"
+                value={formValues.user_name}
+                onChange={handleChange}
+                disabled={true}
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <FormControl fullWidth>
+                <InputLabel>Subsidiary</InputLabel>
+                <Select
+                  name="subsidary"
+                  value={formValues.subsidary}
+                  onChange={handleChange}
                   disabled={disableInputs}
-                  max={getMaxDate()}
-                />
-              </div>
-            </div>
-            <div className="col-12 col-md-6">
-              <div className="mb-3">
-                <textarea
-                  className="form-control"
-                  id="status"
-                  placeholder="Status"
-                  value={formValues.status}
-                  onChange={handleInputChange}
-                  disabled={disableInputs}
-                  rows={1}
-                />
-              </div>
-            </div>
-          </div>
-          <div className="col-12 d-flex justify-content-center gap-3">
+                >
+                  {subsidiaryOptions.map((option) => (
+                    <MenuItem key={option} value={option}>
+                      {option}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                label="Date"
+                type="date"
+                name="date"
+                value={formValues.date}
+                onChange={handleChange}
+                disabled={disableInputs}
+                InputLabelProps={{ shrink: true }}
+                variant="outlined"
+                inputProps={{ max: getMaxDate() }}
+              />
+            </Grid>
+            {/* Dynamic Fields */}
+            {renderSubsidiaryFields()}
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                label="Description"
+                name="description"
+                value={formValues.description}
+                onChange={handleChange}
+                disabled={disableInputs}
+                multiline
+                rows={4}
+                variant="outlined"
+              />
+            </Grid>
+          </Grid>
+          <div className="col-12 d-flex justify-content-center gap-3 py-2">
             <button
               type="submit"
               className="btn btn-primary"
