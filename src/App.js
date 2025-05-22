55bcc2e0c83647d984a25b5b5fcd48a00812880f
Y.Sai Sampreeth <saisampreeth18@gmail.com>
2025-05-22 20:35:26 +0530

    ga analytics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types'; // Add this line
-
+import ReactGA from "react-ga";
 import './App.css';
 import Navbar from './components/organisms/Navbar';
 import Footer from './components/organisms/Footer';
+
+ReactGA.initialize("G-2TGKYMF25W");
 
 export function MainLayout({ children, logout }) {
   return (
