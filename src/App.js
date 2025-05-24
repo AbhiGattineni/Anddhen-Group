f08ad27ae987c4819cfa34085922229dfe61ee1a
Y.Sai Sampreeth <saisampreeth18@gmail.com>
2025-05-24 19:03:50 +0530

    error resolving

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types'; // Add this line
-import ReactGA from "react-ga";
 import './App.css';
 import Navbar from './components/organisms/Navbar';
 import Footer from './components/organisms/Footer';
-
-ReactGA.initialize("G-2TGKYMF25W");
 
 export function MainLayout({ children, logout }) {
   return (
--- src/components/organisms/ImageSlider/ImageSlider.jsx
+++ src/components/organisms/ImageSlider/ImageSlider.jsx
@@ -21,13 +21,7 @@ const ImageSlider = ({ title, cards }) => {
     setStartIndex(0);
   }, [isExtraLargeScreen, isLargeScreen, isMediumScreen, isSmallScreen]);
 
-  const imagesPerPage = isExtraLargeScreen
-    ? 4
-    : isLargeScreen
-      ? 3
-      : isMediumScreen
-        ? 2
-        : 1;
+  const imagesPerPage = isExtraLargeScreen ? 4 : isLargeScreen ? 3 : isMediumScreen ? 2 : 1;
   const totalImages = cards.length;
 
   // Scroll Functionality for Mouse Wheel
